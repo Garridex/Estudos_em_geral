7fd10cb81e9e6c1a24044e99ddc312f732ceb525
Dev3-egos <81248353+dev3-egosti@users.noreply.github.com>
2021-06-22 15:52:56 -0300

    estudos

--- meus testes/sctipt.js
+++ meus testes/sctipt.js
@@ -1,1 +1,16 @@
+var lebron = window.document.getElementById('lebron')
+var kobe = window.document.getElementById('kobe')
 
+var botao = window.document.getElementById('dunk')
+function lebron(){
+
+}
+function kobe(){
+
+}
+
+function dunk(){
+lebron.removeAttribute('hidden')
+kobe.removeAttribute('hidden')
+botao.setAttribute('hidden')
+}
