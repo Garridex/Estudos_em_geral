a23e3059b83f8c700c6ec1474b3a5a2f706389cf
Dev3-egos <81248353+dev3-egosti@users.noreply.github.com>
2021-06-23 11:52:39 -0300

    alterações para ficar todos os estudos na mesma pasta

---
+++ mvc/mvc/app/controller/pages/home.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\controler\pages;
+
+class home{
+
+    /**
+ * metodo responsavel por retornar o conteudo de home
+ * @return string
+ */
+    public static function getHome(){
+        return 'Olá mundo';
+    }
+}
+
+?>
