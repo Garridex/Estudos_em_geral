0919de68cdd9b47890f6409a5b8864e374cbaddf
Dev3-egos <81248353+dev3-egosti@users.noreply.github.com>
2021-06-25 11:20:16 -0300

    Estudos do dia 25/06/21

---
+++ AJAX/teste_upload/controller.php
@@ -0,0 +1,5 @@
+<?php
+$sourcePath = $_FILES['file']['tmp_name'];       // Storing source path of the file in a variable
+$targetPath = "arquivos/".$_FILES['file']['name']; // Target path where file is to be stored
+move_uploaded_file($sourcePath,$targetPath) ;    // Moving Uploaded file
+?>
